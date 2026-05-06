bef0206c0a8ec9b3ceb2a9b130007cd883eec369
Rafael Lopes de Souza da Fonseca <rafaellopessouzafonseca@gmail.com>
2026-05-06 15:51:53 -0300

    Refatorando cards de venda

--- Codigo/backend/src/domain/inventory/dto/user/product/user-product-list.dto.ts
+++ Codigo/backend/src/domain/inventory/dto/user/product/user-product-list.dto.ts
@@ -52,5 +52,12 @@ export class UserProductListResponseDto {
   @ApiProperty()
   total: number;
 
+@ApiProperty()
+  page: number;
 
+  @ApiProperty()
+  itemsPerPage: number;
+
+  @ApiProperty()
+  totalPages: number;
 }
