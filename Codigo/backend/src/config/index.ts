cc7637e8e2eb862de5ca6987d1875a632bf47e41
Rafael Lopes de Souza da Fonseca <rafaellopessouzafonseca@gmail.com>
2026-06-03 19:02:12 -0300

    Add newline at end of index.ts
    Fix missing newline at end of file in index.ts

--- Codigo/backend/src/config/index.ts
+++ Codigo/backend/src/config/index.ts
@@ -3,4 +3,4 @@ import databaseConfig from './database.config';
 import jwtConfig from './jwt.config';
 import mercadoPagoConfig from '../application/payment/providers/mercadopago/mercadopago.config';
 
-export default [appConfig, databaseConfig, jwtConfig, mercadoPagoConfig];
+export default [appConfig, databaseConfig, jwtConfig, mercadoPagoConfig]
